17a583a96117ca565744aee9fcc81c8be53dd337
Yong Jia <yjtoro@hotmail.com>
2016-07-26 14:35:44 +0800

    updated using .html instead of jade

--- server.js
+++ server.js
@@ -1,20 +1,25 @@
-var express = require('express')
-  , logger = require('morgan')
-  , app = express()
-  , template = require('jade').compileFile(__dirname + '/source/templates/homepage.jade')
+var express = require('express');
+var app = express();
+var path = require('path');
+//app.use(logger('dev'))
+//app.use(express.static(__dirname + '/view'));
+app.use(express.static(__dirname + '/static'));
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/view/homepage.html'));
+});
 
-app.use(logger('dev'))
-app.use(express.static(__dirname + '/static'))
+app.get('/news', function (req, res) {
+    res.sendFile(path.join(__dirname + '/view/news.html'));
+});
 
-app.get('/', function (req, res, next) {
-  try {
-    var html = template({ title: 'Home' })
-    res.send(html)
-  } catch (e) {
-    next(e)
-  }
-})
+app.get('/about', function (req, res) {
+   res.sendFile(path.join(__dirname + '/view/about.html'));
+});
+
+app.get('/contact', function (req, res) {
+    res.sendFile(path.join(__dirname + '/view/contact.html'));
+});
 
 app.listen(process.env.PORT || 3000, function () {
-  console.log('Listening on http://localhost:' + (process.env.PORT || 3000))
+    console.log('Listening on http://localhost:' + (process.env.PORT || 3000))
 })
